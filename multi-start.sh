9adcf4dc7433399af33bf115bf49d81dc10a2ef2
root <root@sc-r10rb10-s13.cerebrassc.local>
2022-12-14 07:09:05 -0800

    Add multi-process example using ip addresses

--- multi-start.sh
+++ multi-start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-conf=multi-config.ini
+conf=multi-config
 bin=./multi-example/helloworld2
 
-for((proc_id=0; proc_id<8; ++proc_id))
+for((proc_id=0; proc_id<4; ++proc_id))
 do
     if ((proc_id == 0))
     then
-        echo "${bin} --conf ${conf} --proc-type=primary --proc-id=${proc_id} ${proc_id}"
-        ${bin} --conf ${conf} --proc-type=primary --proc-id=${proc_id} ${proc_id} &
+        echo "${bin} --conf ${conf}${proc_id}.ini --proc-type=primary --proc-id=${proc_id} ${proc_id}"
+        ${bin} --conf ${conf}${proc_id}.ini --proc-type=primary --proc-id=${proc_id} ${proc_id} &
         sleep 5
     else
-        echo "${bin} --conf ${conf} --proc-type=secondary --proc-id=${proc_id} ${proc_id}"
-        ${bin} --conf ${conf} --proc-type=secondary --proc-id=${proc_id} ${proc_id} &
+        echo "${bin} --conf ${conf}${proc_id}.ini --proc-type=secondary --proc-id=${proc_id} ${proc_id}"
+        ${bin} --conf ${conf}${proc_id}.ini --proc-type=secondary --proc-id=${proc_id} ${proc_id} &
     fi
 done
+
+ff_ifconfig f-stack-0 10.250.136.101 netmask 255.255.255.255 alias
+ff_ifconfig f-stack-0 10.250.136.102 netmask 255.255.255.255 alias
+ff_ifconfig f-stack-0 10.250.136.103 netmask 255.255.255.255 alias
